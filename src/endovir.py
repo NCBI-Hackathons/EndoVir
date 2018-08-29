eb90d0cbc8fd46df9a69e36640178da557bb1a76
jpb <jan.buchmann@sydney.edu.au>
2018-08-29 13:23:22 +1000

    feat: Endovir() parses config and initializes tools and databases

--- src/endovir.py
+++ src/endovir.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-#  endovir.py
-#
-#  Author: Jan Piotr Buchmann <jan.buchmann@sydney.edu.au>
-#  Description:
-#
-#  Version: 0.0
-
+#-------------------------------------------------------------------------------
+# \author Jan Piotr Buchmann <jan.buchmann@sydney.edu.au>
+# \copyright 2017,2018 The University of Sydney
+# \description
+#-------------------------------------------------------------------------------
 
 import os
 import sys
@@ -20,7 +17,7 @@ import argparse
 
 #import screener
 #import virus_contig
-
+import status.endovir_status
 import utils.endovir_utils
 import toolbox.endovir_toolbox
 import biodb.biodb_manager
@@ -28,26 +25,31 @@ import biodb.biodb_manager
 
 class Endovir:
 
-  class Config:
+  status_codes = status.endovir_status.EndovirStatusManager.set_status_codes(['CFGERR'])
+  toolbox = toolbox.endovir_toolbox.EndovirToolbox()
+  dbmanager = None
 
-    def __init__(self, config):
-      self.wd = config.pop('wd')
-      if not os.path.isabs(self.wd):
-        self.wd = os.path.join(os.getcwd(), self.wd)
-      utils.endovir_utils.make_dir(self.wd)
-      self.dbdir = os.path.join(self.wd, config.pop('dbdirectory'))
-      utils.endovir_utils.make_dir(self.dbdir)
-      self.flank_len = config.pop('flank_length')
-
-  def __init__(self, config_file):
-    config = json.load(config_file)
-    self.config = self.Config(config['analysis'])
-    self.toolbox = toolbox.endovir_toolbox.EndovirToolbox()
-    self.toolbox.configure(config['tools'])
-    self.dbmanager = biodb.biodb_manager.BiodbManager(self.toolbox)
-    self.dbmanager.configure(self.config.dbdir, config['databases'])
-    self.dbmanager.test_databases()
+  def __init__(self):
     self.screens = {}
+    self.wd = None
+    self.flank_len = None
+    self.status = status.endovir_status.EndovirStatusManager(Endovir.status_codes)
+
+  def load_configuration(self, config_file):
+    if config_file == None:
+      self.parse_configuration(json.load(sys.stdin))
+    else:
+      fh = open(config_file, 'r')
+      self.parse_configuration(json.load(fh))
+      fh.close()
+
+  def parse_configuration(self, config):
+    self.wd = config['analysis']['wd']
+    self.flank_len = config['analysis']['flank_length']
+    Endovir.toolbox.initialize_tools(config['tools'])
+    Endovir.dbmanager = biodb.biodb_manager.BiodbManager(self.wd)
+    Endovir.dbmanager.initialize_databases(config['databases'])
+    print("Endovir  is GO")
 
   # def initial_screen():
   #   return contigs
@@ -80,22 +82,18 @@ class Endovir:
 
 def main():
   ap = argparse.ArgumentParser(description='Endovir')
-  ap.add_argument('-srr', type=str, default='SRR5150787',
-                  help='SRR number, e.g. SRR5150787'),
-  ap.add_argument('-c', '--config', type=argparse.FileType('r'),
+  ap.add_argument('-srr',
+                  type=str,
+                  default='SRR5150787',
+                  help='SRR accession, e.g. SRR5150787'),
+  ap.add_argument('-c', '--config',
+                  type=str,
+                  default=None,
                   help='endovir JSON config file')
   args = ap.parse_args()
 
-  #srrs = ['SRR5150787', 'SRR5832142']
-  #if args.srr == 'SRR5150787':
-    #print("Running test in {} using {}".format(args.wd, args.srr), file=sys.stderr)
-  #else:
-    #print("Analyzing  {} using {}.".format(args.srr, args.wd), file=sys.stderr)
-  ev = Endovir(args.config)
-  #e = Endovir(wd=args.wd)
-  #print("Checking databases", file=sys.stderr)
-  #e.setup()
-  #print("Starting screen", file=sys.stderr)
+  ev = Endovir()
+  ev.load_configuration(args.config)
   #e.screen([args.srr])
   return 0
 
