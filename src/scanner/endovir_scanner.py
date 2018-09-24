4ed2ec678e01391549ab95d3eeba4120a045c0c7
jpb <jan.buchmann@sydney.edu.au>
2018-09-24 12:44:11 +1000

    feat: adjust for changes in individual tools

--- src/scanner/endovir_scanner.py
+++ src/scanner/endovir_scanner.py
@@ -24,24 +24,30 @@ class EndovirScanner:
     self.asm_dir = screen.asm_dir
     self.virus_db = screen.virus_db
     self.status = status.endovir_status.EndovirStatusManager(EndovirScanner.status_codes)
+    self.contigs = {}
+    self.ctg_num = 0
 
   def initial_scan(self, mapper='magicblast', assembler='megahit'):
-    #srr_mapper = toolbox.endovir_toolbox.EndovirToolbox.get_by_name(mapper)
-    #srr_mapper.configure(self.get_settings())
-    #proc = srr_mapper.assemble_process()
-    ##staus checkpoint
-    #investigator = srr_mapper.run(proc)
-    ##staus checkpoint
-    #vdbtool = toolbox.vdb_tool.EndovirVdbTool()
-    #reads = vdbtool.fetch_reads(self.srr, investigator.mapping_result)
-    ##staus checkpoint
+    srr_mapper = toolbox.endovir_toolbox.EndovirToolbox.get_by_name(mapper)
+    srr_mapper.configure(self.get_settings())
+    #status checkpoint
+    investigator = srr_mapper.run(srr_mapper.assemble_process())
+    #status checkpoint
+    vdbtool = toolbox.vdb_tool.EndovirVdbTool(overwrite=False)
+    reads = vdbtool.fetch_reads(self.srr, investigator.get_result())
+    #status checkpoint
     asm = toolbox.endovir_toolbox.EndovirToolbox.get_by_name(assembler)
     asm.configure(self.get_settings())
-    #asm.add_reads(reads)
-    proc = asm.assemble_process()
-    investigator = asm.run(proc)
-    investigator.parse_contigs()
-    #staus checkpoint
+    asm.add_reads(reads)
+    investigator = asm.run(asm.assemble_process())
+    self.contigs.update(investigator.get_result())
+    for i in self.contigs:
+      self.contigs[i].metadata = {}
+      self.contigs[i].metadata['asm_name'] = self.contigs[i].name
+      self.contigs[i].name = "{0}-ctg-{1}".format(self.srr, self.ctg_num)
+      self.ctg_num += 1
+    #status checkpoint
+    print(self.contigs)
 
   def get_settings(self):
     return {
