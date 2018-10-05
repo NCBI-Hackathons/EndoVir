d34868906def3f870234557ec2fc4c3968f68f33
jpb <jan.buchmann@sydney.edu.au>
2018-10-05 11:25:58 +1000

    feat: simplify contig database to dictionary

--- src/scanner/endovir_scanner.py
+++ src/scanner/endovir_scanner.py
@@ -12,7 +12,7 @@ import toolbox.vdb_tool
 import biodb.blastdb
 import biodb.biodb_manager
 import contig.virus_contig
-import contig.contigdb
+import bud.flank_extender
 
 class EndovirScanner:
 
@@ -20,7 +20,7 @@ class EndovirScanner:
 
   def __init__(self, screen):
     self.screen = screen
-    self.virus_contigs = contig.contigdb.ContigDatabase(screen)
+    self.virus_contigs = {}
     self.flankdb = biodb.blastdb.BlastDatabase(name='endovir_flankdb',
                                                dbdir=os.path.join(screen.wd, 'endovir_dbs'),
                                                dbformat='blast',
@@ -47,22 +47,22 @@ class EndovirScanner:
     fdb_proc = fdb_tool.assemble_process()
     for i in ctg_screener.run(ctg_screener.assemble_process()).get_result().get_queries():
       if evctgs.contains(i.id):
-        c = self.virus_contigs.add_contig(contig.virus_contig.VirusContig(evctgs.get_sequence(i.id), self.screen))
-        fdb_proc.stdin.write(utils.fasta_formatter.FastaFormatter.format(c.lhs_flank.sequence))
-        fdb_proc.stdin.write(utils.fasta_formatter.FastaFormatter.format(c.rhs_flank.sequence))
+        self.virus_contigs[i.id] = contig.virus_contig.VirusContig(evctgs.get_sequence(i.id), self.screen)
+        fdb_proc.stdin.write(utils.fasta_formatter.FastaFormatter.format(self.virus_contigs[i.id].lhs_flank.sequence))
+        fdb_proc.stdin.write(utils.fasta_formatter.FastaFormatter.format(self.virus_contigs[i.id].rhs_flank.sequence))
       else:
         evctgs.remove_sequence(i.id)
     fdb_proc.stdin.close()
     fdb_tool.run(fdb_proc)
     #status checkpoint
-    for i in self.virus_contigs.contigs_map:
-      print(i, self.virus_contigs.contigs_map[i].length)
-      print("LHS: len: {0}\tstart: {1}\tstop: {2}".format(self.virus_contigs.contigs_map[i].lhs_flank.get_length(),
-                                                          self.virus_contigs.contigs_map[i].lhs_flank.get_start_pos(),
-                                                          self.virus_contigs.contigs_map[i].lhs_flank.get_end_pos()))
-      print("RHS: len: {0}\tstart: {1}\tstop: {2}".format(self.virus_contigs.contigs_map[i].rhs_flank.get_length(),
-                                                          self.virus_contigs.contigs_map[i].rhs_flank.get_start_pos(),
-                                                          self.virus_contigs.contigs_map[i].rhs_flank.get_end_pos()))
+    for i in self.virus_contigs:
+      print(i, self.virus_contigs[i].length)
+      print("LHS: len: {0}\tstart: {1}\tstop: {2}".format(self.virus_contigs[i].lhs_flank.get_length(),
+                                                          self.virus_contigs[i].lhs_flank.get_start_pos(),
+                                                          self.virus_contigs[i].lhs_flank.get_end_pos()))
+      print("RHS: len: {0}\tstart: {1}\tstop: {2}".format(self.virus_contigs[i].rhs_flank.get_length(),
+                                                          self.virus_contigs[i].rhs_flank.get_start_pos(),
+                                                          self.virus_contigs[i].rhs_flank.get_end_pos()))
 
   def make_endovir_contigs(self, contigs):
     ctg_num = 0
@@ -77,5 +77,5 @@ class EndovirScanner:
   def bud(self, sra_mapper):
     mapper = toolbox.endovir_toolbox.EndovirToolbox.get_by_name(sra_mapper)
     mapper.configure({'sra':self.screen.srr, 'db':self.flankdb.dbpath})
-    flankmap = mapper.run(mapper.assemble_process()).get_result()
-    print(flankmap)
+    fle = bud.flank_extender.FlankExtender()
+    fle.extend(mapper.run(mapper.assemble_process()).get_result())
